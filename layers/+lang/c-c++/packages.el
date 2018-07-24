b5999c59eebe0aa66fcccb2d035f6ae31fd31fb3
Mephis Pheies <mephistommm@gmail.com>
2018-07-24 11:34:13 +0800

    fixbug: delete realgud

--- layers/+lang/c-c++/packages.el
+++ layers/+lang/c-c++/packages.el
@@ -16,7 +16,6 @@
     flycheck
     gdb-mi
     google-c-style
-    realgud
     srefactor
     stickyfunc-enhance
     xcscope
@@ -75,58 +74,6 @@
      gdb-many-windows t
      ;; Non-nil means display source file containing the main routine at startup
      gdb-show-main t)))
-
-(defun c-c++/init-realgud()
-  (use-package realgud
-    :defer t
-    :commands (realgud:gdb)
-    :init
-    (progn
-      (setq rtags-autostart-diagnostics t)
-      (add-hook 'rtags-jump-hook 'evil-set-jump)
-      (rtags-diagnostics)
-      ;; key bindings
-      (evil-define-key 'normal rtags-mode-map
-        (kbd "RET")   'rtags-select-other-window
-        (kbd "M-RET") 'rtags-select
-        (kbd "q")     'rtags-bury-or-delete)
-      ;; TODO check for consistency with gtags key bindings
-      ;; see https://github.com/syl20bnr/spacemacs/blob/develop/layers/+tags/gtags/funcs.el#L70
-      (dolist (mode c-c++-modes)
-        (spacemacs/set-leader-keys-for-major-mode mode
-          "g." 'spacemacs/c-c++-tags-find-symbol-at-point
-          "g," 'spacemacs/c-c++-tags-find-references-at-point
-          "g;" 'spacemacs/c-c++-tags-find-file
-          "g/" 'rtags-find-all-references-at-point
-          "g[" 'rtags-location-stack-back
-          "g]" 'rtags-location-stack-forward
-          "g>" 'spacemacs/c-c++-tags-find-symbol
-          "g<" 'spacemacs/c-c++-tags-find-references
-          "gB" 'rtags-show-rtags-buffer
-          "gd" 'rtags-print-dependencies
-          "gD" 'rtags-diagnostics
-          "ge" 'rtags-reparse-file
-          "gE" 'rtags-preprocess-file
-          "gF" 'rtags-fixit
-          "gG" 'rtags-guess-function-at-point
-          "gh" 'rtags-print-class-hierarchy
-          "gI" 'spacemacs/c-c++-tags-imenu
-          "gL" 'rtags-copy-and-print-current-location
-          "gM" 'rtags-symbol-info
-          "gO" 'rtags-goto-offset
-          "gp" 'rtags-set-current-project
-          "gR" 'rtags-rename-symbol
-          "gs" 'rtags-print-source-arguments
-          "gS" 'rtags-display-summary
-          "gT" 'rtags-taglist
-          "gv" 'rtags-find-virtuals-at-point
-          "gV" 'rtags-print-enum-value-at-point
-          "gX" 'rtags-fix-fixit-at-point
-          "gY" 'rtags-cycle-through-diagnostics)))))
-
-(defun c-c++/post-init-realgud()
-  (dolist (mode c-c++-modes)
-    (spacemacs/add-realgud-debugger mode "gdb")))
 
 (defun c-c++/init-google-c-style ()
   (use-package google-c-style
