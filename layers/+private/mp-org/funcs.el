0436c1705406154393047b8d83586d2c83a08004
MephisPheies <mephistommm@gmail.com>
2018-11-02 08:21:41 +0800

    modify js2 code type to js in mp-org/src-code-types

--- layers/+private/mp-org/funcs.el
+++ layers/+private/mp-org/funcs.el
@@ -102,7 +102,7 @@ user-config should be defined in this function!"
 ;;;; Source Code
 
 (defconst mp-org/src-code-types
-  '("emacs-lisp" "python" "c" "shell" "java" "js2" "clojure" "c++" "css" "go" "rust" "sh" "sass" "sql" "awk" "haskell" "latex" "lisp" "matlab" "org" "perl" "ruby" "scheme" "sqlite" "yaml"))
+  '("emacs-lisp" "python" "c" "shell" "java" "js" "clojure" "c++" "css" "go" "rust" "sh" "sass" "sql" "awk" "haskell" "latex" "lisp" "matlab" "org" "perl" "ruby" "scheme" "sqlite" "yaml"))
 (defvar mp-org--org-insert-src-code-block-history nil)
 
 (defun mp-org/org-insert-src-code-block (src-code-type)
