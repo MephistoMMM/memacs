67c03d7ab80e5214f20fc54019cc15602b4e2054
Mephis Pheies <mephistommm@gmail.com>
2019-01-06 22:35:52 +0800

    config keybinding of memacs/switch-buffer; defer evil-mc

--- layers/+misc/multiple-cursors/packages.el
+++ layers/+misc/multiple-cursors/packages.el
@@ -18,12 +18,9 @@
 
 (defun multiple-cursors/init-evil-mc ()
   (use-package evil-mc
-    :config
+    :defer t
+    :init
     (progn
-      (setq evil-mc-one-cursor-show-mode-line-text nil)
-      (when (or (spacemacs/system-is-mac) (spacemacs/system-is-mswindows))
-        (setq evil-mc-enable-bar-cursor nil))
-
       ;; evil-mc is not compatible with the paste transient state
       (define-key evil-normal-state-map "p" 'spacemacs/evil-mc-paste-after)
       (define-key evil-normal-state-map "P" 'spacemacs/evil-mc-paste-before)
@@ -32,5 +29,19 @@
         (dolist (keybinding `((,(kbd "C-M-j") . evil-mc-make-cursor-move-next-line)
                               (,(kbd "C-M-k") . evil-mc-make-cursor-move-prev-line)))
           (define-key state-map (car keybinding) (cdr keybinding))))
+
       (add-hook 'prog-mode-hook 'turn-on-evil-mc-mode)
-      (add-hook 'text-mode-hook 'turn-on-evil-mc-mode))))
+      (add-hook 'text-mode-hook 'turn-on-evil-mc-mode))
+    :config
+    (progn
+      (setq evil-mc-one-cursor-show-mode-line-text nil)
+      (when (spacemacs/system-is-mac)
+        (setq evil-mc-enable-bar-cursor nil))
+
+      (dolist (keydata '(("C-n" . nil )
+                         ("C-t" . nil )
+                         ("C-p" . nil)))
+        (evil-define-key 'normal evil-mc-key-map (kbd (car keydata)) nil)
+        (evil-define-key 'visual evil-mc-key-map (kbd (car keydata)) nil))
+      ))
+  )
