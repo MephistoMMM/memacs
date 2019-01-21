4bf2359aa019aef0fe8d1c294ff2836220b9ebf6
Mephis Pheies <mephistommm@gmail.com>
2019-01-21 23:38:44 +0800

    add pretty-symbols for emacs, org-mode and go-mode

--- layers/+misc/pretty-code/packages.el
+++ layers/+misc/pretty-code/packages.el
@@ -19,7 +19,31 @@
   (spacemacs|do-after-display-system-init
     (use-package pretty-code
       :config
-      (+pretty-code|setup-fira-ligatures)))
+      (progn
+        (+pretty-code|setup-fira-ligatures)
+        (set-pretty-symbols! 'emacs-lisp-mode
+          :lambda "lambda"
+          :true "t"
+          :false "nil"
+          :def "defun"
+          :and "and"
+          :or "or"
+          :not "not")
+        (set-pretty-symbols! 'org-mode
+          :src_block "#+BEGIN_SRC"
+          :src_block_end "#+END_SRC")
+        (set-pretty-symbols! 'go-mode
+          :null "nil"
+          :def  "func"
+          :true "true"
+          :false "false"
+          :bool  "bool"
+          :and "&&"
+          :or "||"
+          :not "!")
+        ;; TODO define pretty-code-symbols by set-pretty-symbol! function and  `spacemacs/defer-until-after-user-config'
+        )
+      ))
   )
 
 ;;; packages.el ends here
