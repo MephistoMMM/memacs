4f308b350888202efadf5a4c0924ff742ed4d4b9
Mephis Pheies <mephistommm@gmail.com>
2019-06-26 23:46:37 +0800

    fixbug: company-box show images

--- layers/+misc/pretty-code/config.el
+++ layers/+misc/pretty-code/config.el
@@ -28,43 +28,48 @@
                               memacs-company-box-backend-yasnippet-selected-face)))
       )
 
-(setq company-box-icons-unknown 'fa_question_circle)
+;; FIXME replace icons-in-terminal to all-the-icons
+(setq company-box-icons-alist 'company-box-icons-icons-in-terminal)
 
-(setq company-box-icons-elisp
-      '((fa_tag        :height 1 :face font-lock-function-name-face) ;; Function
-        (fa_cog        :height 1 :face font-lock-variable-name-face) ;; Variable
-        (fa_cube       :height 1 :face font-lock-constant-face     ) ;; Feature
-        (md_color_lens :height 1 :face font-lock-doc-face          ) ;; Face
-        ))
+;; (setq company-box-icons-unknown 'fa_question_circle)
+
+;; (setq company-box-icons-elisp
+;;       '((fa_tag        :height 1 :face font-lock-function-name-face) ;; Function
+;;         (fa_cog        :height 1 :face font-lock-variable-name-face) ;; Variable
+;;         (fa_cube       :height 1 :face font-lock-constant-face     ) ;; Feature
+;;         (md_color_lens :height 1 :face font-lock-doc-face          ) ;; Face
+;;         ))
 
 (setq company-box-icons-yasnippet 'fa_bookmark)
 
-(setq company-box-icons-lsp
-      '((1  . (fa_text_height              :height 1                                   )) ;; Text
-        (2  . (fa_tags                     :height 1 :face font-lock-function-name-face)) ;; Method
-        (3  . (fa_tag                      :height 1 :face font-lock-function-name-face)) ;; Function
-        (4  . (fa_tag                      :height 1 :face font-lock-function-name-face)) ;; Constructor
-        (5  . (fa_cog                      :height 1 :face font-lock-builtin-face      )) ;; Field
-        (6  . (fa_cog                      :height 1 :face font-lock-variable-name-face)) ;; Variable
-        (7  . (fa_cube                     :height 1 :face font-lock-preprocessor-face )) ;; Class
-        (8  . (fa_puzzle_piece             :height 1 :face font-lock-preprocessor-face )) ;; Interface
-        (9  . (fa_cube                     :height 1 :face font-lock-preprocessor-face )) ;; Module
-        (10 . (fa_cog                      :height 1 :face font-lock-builtin-face      )) ;; Property
-        (11 . (md_settings_system_daydream :height 1                                   )) ;; Unit
-        (12 . (fa_cog                      :height 1 :face default                     )) ;; Value
-        (13 . (md_storage                  :height 1 :face font-lock-type-face         )) ;; Enum
-        (14 . (md_closed_caption           :height 1 :face font-lock-keyword-face      )) ;; Keyword
-        (15 . (md_closed_caption           :height 1                                   )) ;; Snippet
-        (16 . (md_color_lens               :height 1 :face font-lock-doc-face          )) ;; Color
-        (17 . (fa_file_text_o              :height 1                                   )) ;; File
-        (18 . (md_refresh                  :height 1                                   )) ;; Reference
-        (19 . (fa_folder_open              :height 1                                   )) ;; Folder
-        (20 . (md_closed_caption           :height 1 :face font-lock-builtin-face      )) ;; EnumMember
-        (21 . (fa_square                   :height 1 :face font-lock-constant-face     )) ;; Constant
-        (22 . (fa_cube                     :height 1 :face font-lock-type-face         )) ;; Struct
-        (23 . (fa_calendar                 :height 1                                   )) ;; Event
-        (24 . (fa_square_o                 :height 1                                   )) ;; Operator
-        (25 . (fa_arrows                   :height 1                                   )) ;; TypeParameter
+(setq company-box-icons-icons-in-terminal
+      '((Unknown . (fa_question_circle       :height 1                                   ))
+        (Text  . (fa_text_height             :height 1                                   ))
+        (Method  . (fa_tags                  :height 1 :face font-lock-function-name-face))
+        (Function  . (fa_tag                 :height 1 :face font-lock-function-name-face))
+        (Constructor  . (fa_tag              :height 1 :face font-lock-function-name-face))
+        (Field  . (fa_cog                    :height 1 :face font-lock-builtin-face      ))
+        (Variable  . (fa_cog                 :height 1 :face font-lock-variable-name-face))
+        (Class  . (fa_cube                   :height 1 :face font-lock-preprocessor-face ))
+        (Interface  . (fa_puzzle_piece       :height 1 :face font-lock-preprocessor-face ))
+        (Module  . (fa_cube                  :height 1 :face font-lock-preprocessor-face ))
+        (Property . (fa_cog                  :height 1 :face font-lock-builtin-face      ))
+        (Unit . (md_settings_system_daydream :height 1                                   ))
+        (Value . (fa_cog                     :height 1 :face default                     ))
+        (Enum . (md_storage                  :height 1 :face font-lock-type-face         ))
+        (Keyword . (md_closed_caption        :height 1 :face font-lock-keyword-face      ))
+        (Snippet . (md_closed_caption        :height 1                                   ))
+        (Color . (md_color_lens              :height 1 :face font-lock-doc-face          ))
+        (File . (fa_file_text_o              :height 1                                   ))
+        (Reference . (md_refresh             :height 1                                   ))
+        (Folder . (fa_folder_open            :height 1                                   ))
+        (EnumMember . (md_closed_caption     :height 1 :face font-lock-builtin-face      ))
+        (Constant . (fa_square               :height 1 :face font-lock-constant-face     ))
+        (Struct . (fa_cube                   :height 1 :face font-lock-type-face         ))
+        (Event . (fa_calendar                :height 1                                   ))
+        (Operator . (fa_square_o             :height 1                                   ))
+        (TypeParameter . (fa_arrows          :height 1                                   ))
+        (Template . (fa_bookmark             :height 1                                   ))
         ))
 
 
