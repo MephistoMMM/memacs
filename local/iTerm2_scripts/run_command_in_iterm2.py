c70797cedf72bc94aeac07a34f76b141bfa08cd0
Mephis Pheies <mephistommm@gmail.com>
2019-07-06 17:07:03 +0800

    rewrite "switch to iTerm2 and run command" function by python API

--- local/iTerm2_scripts/run_command_in_iterm2.py
+++ local/iTerm2_scripts/run_command_in_iterm2.py
@@ -17,6 +17,7 @@ async def main(connection):
     app = await iterm2.async_get_app(connection)
     session = app.current_terminal_window.current_tab.current_session
     await session.async_send_text(get_cmd()+"\n")
-    await app.async_active(True, True)
+    await app.async_activate(True, True)
+    print("Done")
 
 iterm2.run_until_complete(main)
