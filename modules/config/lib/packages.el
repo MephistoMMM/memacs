fe8ca5a3a3a4037e8eac6f2c26a2f6edbd8168ee
Mephis Pheies <mephistommm@gmail.com>
2020-01-21 23:18:37 +0800

    defer use package yamlmod

--- modules/config/lib/packages.el
+++ modules/config/lib/packages.el
@@ -4,5 +4,6 @@
 ;; My Dynamic Modules Directory: ~/.emacs.d/.local/dymodules
 
 (use-package! yamlmod-wrapper
+  :defer t
   :commands (yamlmod-load yamlmod-read-file yamlmod-ypath-search)
   :load-path "~/.emacs.d/.local/dymodules/yamlmod")
