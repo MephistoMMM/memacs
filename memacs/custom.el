27b0a417d080fdc5c0a5ccd3fca95167476fd262
Mephis Pheies <mephistommm@gmail.com>
2020-09-27 14:58:26 +0800

    add auth sources

--- memacs/custom.el
+++ memacs/custom.el
@@ -3,6 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-source-gpg-encrypt-to '("mephistommm@gmail.com"))
+ '(auth-sources
+   '("~/Dropbox/dotconf/authinfo.gpg" "~/.authinfo.gpg" "~/.netrc"))
  '(safe-local-variable-values
    '((eval setq-local org-roam-directory
            (concat
