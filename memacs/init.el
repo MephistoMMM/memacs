f0ebbacd60116b40c38d87d1904314050342f016
MephistoMMM <mephistommm@gmail.com>
2025-09-24 23:37:59 +0800

    init: enable tramp

--- memacs/init.el
+++ memacs/init.el
@@ -83,7 +83,7 @@
        electric          ; smarter, keyword-based electric-indent
        ;;eww               ; the internet is gross
        (ibuffer +icons)  ; interactive buffer management
-       ;;tramp             ; remote files at your arthritic fingertips
+       tramp             ; remote files at your arthritic fingertips
        (undo +tree)      ; persistent, smarter undo for your inevitable mistakes
        vc                ; version-control and Emacs, sitting in a tree
 
