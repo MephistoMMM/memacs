93db50c884777ac5f53a25a037fa786ca8ab48ff
MephistoMMM <mephistommm@gmail.com>
2025-09-27 22:59:11 +0800

    squash:5b9978c6366551238befe7f436a53c06d724d2b2..1d1d17e9c910146731cb7b19d5bcbf6c4b283a74  fix(upload): +upload/register-remote: don't overwrite old values  fix(lua): treesit: use tree-sitter-lua@v0.3.0 on <= 29  tweak: do *not* download package archives by default  nit(tramp): fix comment about ssh>scp (part 2)  refactor(default): move whitespace & tramp config  fix(upload): ensure ssh-deploy is loaded  feat(upload): add commands for manual/temporary mappings  feat(upload): activate ssh-deploy-line-mode  docs(upload): revise & clarify  nit(tramp): fix comment about ssh>scp  fix(lib): void-variable type  tweak(lsp): eglot-code-action-indications: remove margin  feat(workspaces): implement tab-local parameters  fix(workspaces): missing tabspaces-session-file parent dir  tweak: default values for tab-*-tab-choice vars  fix(workspaces): unbalances parens  tweak(workspaces): tabspaces-fully-resolve-paths = t  fix(workspaces): projectile/project integration  tweak(workspaces): centralize project session files  fix(dired): remove unneeded persp-mode hack  fix: more vestigial references to persp-mode  fix(everywhere): update for tabspaces  fix(tabs): update for tabspaces  bump: :ui workspaces  fix(workspaces): +workspaces/kill  tweak(workspaces): +workspaces-remove-buffer-from-tab: make tab arg optional  fix(workspaces): +workspaces-buffer-list: include current tab's  fix(nav-flash): update for tabspaces  fix(lib): update for tabspaces  fix(org): update for tabspaces  fix(:term): update for tabspaces  fix(irc): update for tabspaces  fix(ibuffer): update for tabspaces  feat(workspaces): introduce +workspaces-dir  feat(workspaces): expand and polish API  fix(workspaces): remove +workspaces--evil-alternate-buffer-a advice  feat(default): bind 'SPC TAB b' to tabspaces-switch-buffer-and-tab  feat(doom): tab-bar: add basic theming  feat(workspaces): introduce +tabs flag  fix(workspaces): close-window-or-workspace remaps  fix(evil): +evil:kill-all-buffers: update for tabspaces  fix(:completion): update for tabspaces  fix(layout): update workspaces keybinds  fix(workspaces): +workspaces/kill: safeguards  fix(workspaces): leader keybinds  fix(workspaces): vim keybinds & ex commands for tabs  fix(workspaces): evil ex commands  fix(doom-dashboard): replace persp-mode w/ tabspaces  fix(default): update workspaces keybinds  fix(modeline): don't show tabspaces name in modeline  refactor!(workspaces): replace persp-mode w/ tabspaces  Add screenshots

--- modules/tools/upload/config.el
+++ modules/tools/upload/config.el
@@ -1,17 +1,5 @@
 ;;; tools/upload/config.el -*- lexical-binding: t; -*-
 
-;; Uses `ssh-deploy' to map a local folder to a remote one. Set
-;; `ssh-deploy-root-remote' and `ssh-deploy-root-local' in a .dir-locals.el file
-;; to establish this mapping.
-;;
-;; Example:
-;;   ((nil . ((ssh-deploy-root-local . "/local/path/to/project")
-;;            (ssh-deploy-root-remote . "/ssh:user@server:/remote/project/")
-;;            (ssh-deploy-on-explicit-save . 1))))
-;;
-;; Note: `ssh-deploy-root-local' is optional, and will resort to
-;; `doom-project-root' if unspecified.
-
 (use-package! ssh-deploy
   :commands (ssh-deploy-upload-handler
              ssh-deploy-upload-handler-forced
@@ -19,11 +7,12 @@
              ssh-deploy-browse-remote-handler
              ssh-deploy-remote-changes-handler)
   :init
-  (setq ssh-deploy-revision-folder (concat doom-cache-dir "ssh-revisions/")
+  (setq ssh-deploy-revision-folder (file-name-concat doom-cache-dir "ssh-revisions/")
         ssh-deploy-on-explicit-save 1
         ssh-deploy-automatically-detect-remote-changes nil)
 
-  ;; Make these safe as file-local variables
+  ;; Forward-declare these as safe file/dir-local variables in case files set
+  ;; them before ssh-deploy is loaded.
   (dolist (sym '((ssh-deploy-root-local . stringp)
                  (ssh-deploy-root-remote . stringp)
                  (ssh-deploy-script . functionp)
@@ -33,21 +22,29 @@
                  (ssh-deploy-exclude-list . listp)))
     (put (car sym) 'safe-local-variable (cdr sym)))
 
-  ;; Maybe auto-upload on save
+  ;; Respect `ssh-deploy-on-explicit-save' if `ssh-deploy-root-remote' has
+  ;; changed since the buffer was opened.
   (add-hook! 'after-save-hook
     (defun +upload-init-after-save-h ()
       (when (and (bound-and-true-p ssh-deploy-root-remote)
+                 (require 'ssh-deploy nil t)
                  (integerp ssh-deploy-on-explicit-save)
                  (> ssh-deploy-on-explicit-save 0))
-        (ssh-deploy-upload-handler ssh-deploy-force-on-explicit-save))))
+        (ssh-deploy-upload-handler ssh-deploy-force-on-explicit-save)
+        (when (or ssh-deploy-root-remote
+                  ssh-deploy-root-local)
+          (ssh-deploy-line-mode +1)))))
 
   ;; Enable ssh-deploy if variables are set, and check for changes on open file
   ;; (if possible)
   (add-hook! 'find-file-hook
     (defun +upload-init-find-file-h ()
-      (when (bound-and-true-p ssh-deploy-root-remote)
-        (require 'ssh-deploy)
+      (when (and (bound-and-true-p ssh-deploy-root-remote)
+                 (require 'ssh-deploy nil t))
         (unless ssh-deploy-root-local
           (setq ssh-deploy-root-local (doom-project-root)))
         (when ssh-deploy-automatically-detect-remote-changes
-          (ssh-deploy-remote-changes-handler))))))
+          (ssh-deploy-remote-changes-handler))
+        (when (or ssh-deploy-root-remote
+                  ssh-deploy-root-local)
+          (ssh-deploy-line-mode +1))))))
